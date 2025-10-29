b697d57cac1668c15a8aca057ab9a9578cd7693b
Luciano Camargo <karreiradev.lucamargo@gmail.com>
2025-10-29 13:26:13 -0300

    Removal
    Foram removidos pastas  e alterado alguns arquivos para o bom desenvolvimento do projeto que estamos inicializando.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
-import { useState } from "react"; // ✅ Import do hook
-
-function App() {
-  const [count, setCount] = useState(0); // ✅ Uso do hook useState
 
 
-
+export function App() {
+ 
   return (
     <>
       <p>hello word</p>
 
-      <button onClick={() => setCount(count + 1)}>
-        Count {count}
-      </button>
     </>
   )
 }
-
-export default App
